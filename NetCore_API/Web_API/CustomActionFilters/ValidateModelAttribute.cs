04268288c6f4aa3f55717e3c15c8796bc3133e36
Riddhish Rajani <80834752+rajaniriddhish@users.noreply.github.com>
2023-06-19 00:11:48 +0530

    Section6 (#3)
    Added Validation

---
+++ NetCore_API/Web_API/CustomActionFilters/ValidateModelAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web_API.CustomActionFilters
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(context.ModelState.IsValid == false) 
+            {
+                context.Result = new BadRequestResult();
+            }
+        }
+    }
+}
