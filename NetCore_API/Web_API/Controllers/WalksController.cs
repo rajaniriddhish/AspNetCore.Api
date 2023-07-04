801bce2c4c0eeb0b183cdd1b6aff6779c4733ea3
Riddhish Rajani <80834752+rajaniriddhish@users.noreply.github.com>
2023-07-04 15:22:48 +0530

    Feature/section7 (#4)
    * Section 7
    
    Filtering, Sorting, Pagination
    
    * Section7
    
    Add missing file

--- NetCore_API/Web_API/Controllers/WalksController.cs
+++ NetCore_API/Web_API/Controllers/WalksController.cs
@@ -22,12 +22,14 @@ namespace Web_API.Controllers
         }
 
         // Get all walks
-        // GET: http://localhost:port/api/walks
+        // GET: http://localhost:port/api/walks?filterOn=Name&filterQuery=Track&sortBy=Name&isAscending=true
+        // &pageNumber=1&pageSize=10
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery
+            , [FromQuery] string? sortBy, [FromQuery] bool? isAscending, [FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 1000)
         {
             //Get Data from database
-            var walksDomain = await walkRepository.GetAllAsync();
+            var walksDomain = await walkRepository.GetAllAsync(filterOn, filterQuery, sortBy, isAscending ?? true, pageNumber, pageSize);
 
             var walkDto = mapper.Map<List<WalkDto>>(walksDomain);
             return Ok(walkDto);
