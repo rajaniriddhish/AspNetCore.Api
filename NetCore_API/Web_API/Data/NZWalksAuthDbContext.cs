cbcb6306c123c731dfdd59bce97af35bd67a9498
Riddhish Rajani <80834752+rajaniriddhish@users.noreply.github.com>
2023-07-06 15:49:29 +0530

    Section 8 (#5)
    What: Authentication and Authorization JWT token

---
+++ NetCore_API/Web_API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "7c6aec47-a7da-4a1f-8063-0bae61ffac20";
+            var writerRoleId = "e50672d5-d1fa-42f7-8527-740269601143";
+            var roles = new List<IdentityRole>
+            { 
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
