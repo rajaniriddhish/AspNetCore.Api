9fec67d1d4c11ef156dd575d8381fbf568aedfdc
Riddhish Rajani <rrajani@cyzma.com>
2023-07-10 16:51:09 +0530

    Section9
    Image Upload and store it into database.

---
+++ NetCore_API/Web_API/Repositories/IImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+using Web_API.Models.Domain;
+
+namespace Web_API.Repositories
+{
+    public interface IImageRepository
+    {
+        Task<Image> Upload(Image image);
+    }
+}
