b18f62515d5ddb8e5a861cf5c64761d4b73d3824
Riddhish Rajani <80834752+rajaniriddhish@users.noreply.github.com>
2023-07-10 16:53:10 +0530

    Section9 (#7)
    Image Upload and store it into database.

---
+++ NetCore_API/Web_API/Controllers/ImagesController.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Core.Pipeline;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+using Web_API.Models.Domain;
+using Web_API.Models.DTO;
+using Web_API.Repositories;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+        //POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        { 
+            ValidateFileUpload(request);
+            if(ModelState.IsValid)
+            {
+                //Convert DTO to Domain Model
+                var imageDomainModel = new Image 
+                { 
+                    File = request.File,
+                    FileExtention = Path.GetExtension(request.File.FileName),
+                    FileSizeInBytes = request.File.Length,
+                    FileName  = request.FileName,
+                    FileDescription = request.FileDescription,
+                    
+                };
+
+                // User repository to upload image
+                await imageRepository.Upload(imageDomainModel);
+                return Ok(imageDomainModel);
+
+            }
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        { 
+            var allowedExtentions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowedExtentions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extention");
+            }
+
+            if(request.File.Length > 10485760)
+            { 
+                ModelState.AddModelError("file", "File size more then 10MB, PLease upload a smaller size file.");
+            }
+        }
+    }
+}
--- NetCore_API/Web_API/Program.cs
+++ NetCore_API/Web_API/Program.cs
@@ -7,12 +7,15 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => 
@@ -55,6 +58,7 @@ builder.Services.AddDbContext<NZWalksAuthDbContext>(options => options.UseSqlSer
 builder.Services.AddScoped<IRegionRepository, SQLRegoinRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
@@ -102,6 +106,13 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images",
+
+});
 
 app.MapControllers();
 
