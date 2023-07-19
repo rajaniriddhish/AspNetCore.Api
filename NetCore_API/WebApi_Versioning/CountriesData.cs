a5e17cece55b75fc5f5f2625a648afa69bd3f41a
Riddhish Rajani <rrajani@cyzma.com>
2023-07-19 15:56:11 +0530

    Section11
    Create new project for learn versioning.

---
+++ NetCore_API/WebApi_Versioning/CountriesData.cs
@@ -0,0 +1,26 @@
+﻿using WebApi_Versioning.Models.Domain;
+
+namespace WebApi_Versioning
+{
+    public static class CountriesData
+    {
+        public static List<Country> Get()
+        {
+            var countries = new[] 
+            {
+                new { Id = 1, Name = "United States" },
+                new { Id = 2, Name = "Germany" },
+                new { Id = 3, Name = "Brazil" },
+                new { Id = 4, Name = "China" },
+                new { Id = 5, Name = "Italy" },
+                new { Id = 6, Name = "South Africa" },
+                new { Id = 7, Name = "Maxico" },
+                new { Id = 8, Name = "Japan" },
+                new { Id = 9, Name = "Russia" },
+                new { Id = 10, Name = "Australia" },
+            };
+
+            return countries.Select(c => new Country { Id = c.Id, Name = c.Name}).ToList();
+        }
+    }
+}
